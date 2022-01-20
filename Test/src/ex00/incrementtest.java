99574f0bbfb520c6e37d27c2fb30f9de6d215c3d
kamisama <kamisama@youshinn>
2022-01-20 23:57:40 +0900

    1

---
+++ Test/src/ex00/incrementtest.java
@@ -0,0 +1,29 @@
+package ex00;
+
+
+class Car{
+	
+	void run() {
+		
+	}
+}
+
+public class incrementtest {
+	public static void main(String[] args) {
+		byte a = 0;
+		byte b = 0;
+		Car car = new Car();
+		System.out.println();
+//		b = a++;
+		
+		
+		
+//		System.out.println(b);
+//	
+//		System.out.println("=======================");
+//		System.out.println(a);
+	for(byte i = 0; i < 3; ++i) {
+		System.out.println(i);
+	}
+	}
+}
